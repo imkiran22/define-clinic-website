d3618d03cb87aed097da01919d6f2ad024f2ec00
imkiran22 <kiran22292@gmail.com>
2022-09-22 21:25:36 +0530

    Added

--- components/BookAppointment/BookAppointment.tsx
+++ components/BookAppointment/BookAppointment.tsx
@@ -1,41 +1,66 @@
 import React from "react";
 import {
-  PhoneOutlined, MailOutlined, WhatsAppOutlined, PushpinOutlined
-} from '@ant-design/icons';
-import { ADDRESS, MAP, MOBILE, EMAIL, CLINIC_NAME } from "../../constants/clinic-details";
+  PhoneOutlined,
+  MailOutlined,
+  WhatsAppOutlined,
+  PushpinOutlined,
+} from "@ant-design/icons";
+import {
+  ADDRESS,
+  MAP,
+  MOBILE,
+  EMAIL,
+  CLINIC_NAME,
+} from "../../constants/clinic-details";
 
 const BookAppointment: React.FC = () => {
-  return <div className="bookAppointment">
-    <div className="photo">
-      <div className="title"><span>BOOK APPOINTMENT</span></div>
-    </div>
-    <div className="sub-title">For Appointments</div>
-    <div className="content">
-      <div className="wrapper">
-        <div className="contact"><span>Contact Us</span></div>
-        <div className="address">
-          <div><PushpinOutlined /><span>Address:</span>
+  return (
+    <div className="bookAppointment">
+      <div className="photo">
+        <div className="title">
+          <span>BOOK APPOINTMENT</span>
+        </div>
+      </div>
+      <div className="sub-title">For Appointments</div>
+      <div className="content">
+        <div className="wrapper">
+          <div className="contact">
+            <span>Contact Us </span>
           </div>
-          <div className="name">{CLINIC_NAME}</div>
-          <span>{ADDRESS}</span> 
+          <div className="address">
+            <div>
+              <PushpinOutlined />
+              <span>Address:</span>
+            </div>
+            <div className="name">{CLINIC_NAME}</div>
+            <span>{ADDRESS}</span>
+          </div>
+          <div className="phone">
+            <PhoneOutlined /> <span className="value">{MOBILE}</span>
+          </div>
+          <div className="phone whatsapp">
+            <WhatsAppOutlined /> <span className="value">{MOBILE}</span>
+          </div>
+          <div className="email">
+            <MailOutlined /> <span className="value">{EMAIL}</span>
+          </div>
         </div>
-        <div className="phone">
-          <PhoneOutlined /> <span>{MOBILE}</span>
-        </div>
-        <div className="phone whatsapp">
-          <WhatsAppOutlined /> <span>{MOBILE}</span>
-        </div>
-        <div className="email">
-          <MailOutlined /> <span>{EMAIL}</span>
-        </div>
-      </div>
-      <div className="communication wrapper">
-        <div className="map">
-          <iframe src={MAP} width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <div className="communication wrapper">
+          <div className="map">
+            <iframe
+              src={MAP}
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
 };
 
 export default BookAppointment;
--- constants/about.ts
+++ constants/about.ts
@@ -1,4 +1,4 @@
-export const ABOUT_CONTENT = `Define skin hair & laser is a cosmetic skin clinic that was build from the scratch with a passion to provide the best aesthetic care in a more ethical way. 
+export const ABOUT_CONTENT = `Define skin hair & laser clinic is a cosmetic clinic that was build from the scratch with a passion to provide the best aesthetic care in a more ethical way. 
 We started our clinic in this year 2022 opened in avadi. 
 We believe when knowledge and technology comes in right hands, we can bring the right results. 
 Your skin and hair talks so much about you, brings out your confidence to face the world. 
@@ -8,7 +8,7 @@ We will help you look confident and happy in your own skin.
 Good skin and hair happens from within, so just giving you a precription wouldn’t solve it, 
 its important to analyse, find the cause and treat it. 
 We are passionate, young and enthusiastic who believe in science, evidences and promising results. 
-Good skin and hair is science, doesn’t happen overnight, it takes time to learn it!! is a registered brand of Sethu Healthcare LLP. ZI Located at Poes Garden and Anna Nagar offers clinical dermatology & Aesthetic skincare services. We treat with FDA approved lasers and an International range of products. ZI positions itself as a high end state of the art clinic providing affordable treatments .We provide private, personalised and customized care. ZI primarily focuses on hair loss disorder and uses patented techniques for hair transplantation. The serenity, peace and ambience of the clinic gives the patient a sigh of relief.`
+Good skin and hair is science, doesn’t happen overnight, it takes time to learn it!`
 
 export const TITLE = 'ABOUT';
 
