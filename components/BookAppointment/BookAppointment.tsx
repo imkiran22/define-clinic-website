0549a58f37a49ebe425c1a96e48aec19366a5c66
imkiran22 <kiran22292@gmail.com>
2022-09-22 10:22:00 +0530

    Added

--- components/BookAppointment/BookAppointment.tsx
+++ components/BookAppointment/BookAppointment.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import React from "react";
 import {
-  PhoneOutlined, MailOutlined, WhatsAppOutlined
+  PhoneOutlined, MailOutlined, WhatsAppOutlined, PushpinOutlined
 } from '@ant-design/icons';
+import { ADDRESS, MAP, MOBILE, EMAIL } from "../../constants/clinic-details";
 
 const BookAppointment: React.FC = () => {
   return <div className="bookAppointment">
@@ -12,24 +12,24 @@ const BookAppointment: React.FC = () => {
     <div className="title">For Appointments</div>
     <div className="content">
       <div className="wrapper">
+        <div className="contact">Contact Us</div>
+        <div className="address">
+          <div><PushpinOutlined /><span>Address:</span></div>
+          <span>{ADDRESS}</span> 
+        </div>
         <div className="phone">
-          <PhoneOutlined /> <span>+91 6379256207</span>
+          <PhoneOutlined /> <span>{MOBILE}</span>
         </div>
         <div className="phone whatsapp">
-          <WhatsAppOutlined /> <span>+91 6379256207</span>
+          <WhatsAppOutlined /> <span>{MOBILE}</span>
         </div>
         <div className="email">
-          <MailOutlined /> <span>define.skinhair.clinic@gmail.com</span>
+          <MailOutlined /> <span>{EMAIL}</span>
         </div>
       </div>
       <div className="communication wrapper">
-      <div className="address">
-          <div>Address:</div>
-       <span>Define skin hair & laser clinic
-No 2, 89, Poonamallee - Avadi High Rd, Ram Nagar, Township Area, TNHB Mig V Block, Avadi, Tamil Nadu 600054</span> 
-        </div>
         <div className="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.850344624979!2d80.10620691513428!3d13.108665590766073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52636845264889%3A0xb72239b67cb38c34!2sDefine%20skin%20hair%20%26%20laser%20clinic!5e0!3m2!1sen!2sin!4v1663738471657!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src={MAP} width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
