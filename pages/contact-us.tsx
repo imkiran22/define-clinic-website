58b725267086f5b6b27aaa460c521c1b2bb66fb4
imkiran22 <kiran22292@gmail.com>
2022-09-23 10:55:12 +0530

    Added

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Contact from '../components/Contact/Contact';
 
-const ContactUsPage = () => {
-  return <div className='bookAppointment'><Contact/></div>
+const ContactUsPage = ({className = ''}) => {
+  return <div className={`bookAppointment ${className}`}><Contact/></div>
 }
 
 export default ContactUsPage;
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import About from '../components/About/About';
 import AppointmentCard from '../components/Cards/AppointmentCard';
 import Procedures from '../components/Procedures/Procedures';
+import ContactUsPage from './contact-us';
 
 const HomePage = () => {
-  return <><AppointmentCard/><Procedures/><About/></>
+  return <><AppointmentCard/><Procedures/><About/><ContactUsPage className='whitesmoke-bg'/></>
 }
 
 export default HomePage;
