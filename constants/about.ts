512f137d76626aa8a5cf23224e57c6269f120817
imkiran22 <kiran22292@gmail.com>
2023-08-14 10:39:15 +0530

    description and ig handle change

--- constants/about.ts
+++ constants/about.ts
@@ -1,12 +1,14 @@
-export const ABOUT_CONTENT = `Define skin hair & laser clinic is a cosmetic clinic which is commenced with a passion to provide the best aesthetic care. 
-We opened our clinic in avadi in the year 2022. 
-There is certainity when knowledge and technology gets in best hands the brighter results we can foresee. 
-A person's skin and hair will definitely speak the confidence in one self. 
-We are very passionate in bringing the most advanced and efficient aesthetic procedures to the common people at reasonable prices and we believe that everyone deserves to be beautiful. Taking a first step in visiting a doctor for skin and hair concerns itself is a biggest thing for a person and we believe that price of treatments and procedures should not stop them from pursuing their desire to have a healthy skin. Here at Define skin hair and laser clinic, we continuously strive hard for making every cosmetic procedure easily accessible and affordable for everyone.  `;
+export const ABOUT_CONTENT = `Welcome to Define Cosmetic Clinic, an oasis of elegance and rejuvenation founded in 2022 in Avadi. Our commitment to unveiling your true beauty is fueled by a fusion of passion and expertise. We believe in harmonizing knowledge and innovation to provide advanced aesthetic procedures that celebrate your unique identity. Your skin and hair are canvases of confidence, deserving the utmost care and attention.
+
+At Define Cosmetic Clinic, we're driven by a mission to make beauty accessible to all. Financial considerations should never hinder your path to vibrant skin and hair. We're dedicated to shattering those barriers, ensuring that everyone can embrace their desired aesthetic journey.
+
+Stepping into our clinic isn't just an appointment – it's a transformative experience. Let us guide you through personalized treatments that accentuate your natural beauty. Define Cosmetic Clinic isn't just a destination; it's a realm of self-discovery, empowerment, and metamorphosis.
+
+Join us at Define Cosmetic Clinic to redefine beauty. Discover a space where expertise meets innovation, and where your inner radiance is matched by luminous skin and hair. Let's embark on this journey together – each visit propelling you towards the embodiment of your best self.`;
 
 export const TITLE = "ABOUT";
 
-export const FUNCTIONING_TITLE = "FUNCTIONING HOURS";
+export const FUNCTIONING_TITLE = "CONSULTING HOURS";
 
 export const FUNCTIONING_DAYS = [
   {
--- constants/footer.ts
+++ constants/footer.ts
@@ -4,6 +4,6 @@ export const COPY_RIGHT =
 export const FB_PROFILE =
   "https://www.facebook.com/defineskinhairclinic-112627771411004";
 
-export const IG_PROFILE = "https://www.instagram.com/define.skin.hair.clinic";
+export const IG_PROFILE = "https://www.instagram.com/define.cosmeticclinic";
 
 export const FOLLOW_US = "Follow us on";
